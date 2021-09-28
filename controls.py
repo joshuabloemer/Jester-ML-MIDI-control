b7a49620ae041a69a212246119cdbcb1008437a8
joshuabloemer <55506217+joshuabloemer@users.noreply.github.com>
2021-09-28 13:17:37 +0200

    midi pad

--- controls.py
+++ controls.py
@@ -1,18 +1,18 @@
 import time
 import mido
 
-def turn_off(port,faders=(1,48),vel=127):
+def turn_off(out_port,faders=(1,48),vel=127):
     if type(faders) is not tuple:
         faders = (faders,)
     for fader in range(faders[0],faders[-1]+1):
         msg = mido.Message('note_off',note=fader,velocity=vel)
-        port.send(msg)
+        out_port.send(msg)
         time.sleep(0.00001)
 
-def turn_on(port,faders=(1,48),vel=127):
+def turn_on(out_port,faders=(1,48),vel=127):
     if type(faders) is not tuple:
         faders = (faders,)
     for fader in range(faders[0],faders[-1]+1):
         msg = mido.Message('note_on',note=fader,velocity=vel)
-        port.send(msg)
+        out_port.send(msg)
         time.sleep(0.00001)
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import time
 import mido
 from pynput import keyboard
+import playsound
 from controls import turn_on, turn_off
+from threading import Thread
+
 
 out_port = mido.open_output('USB20MIDI 1')
 keys = "1234567890qwertzuiopüasdfghjklöäyxcvbnm"
@@ -20,14 +23,37 @@ def on_release(key):
         # Stop listener
         return False
     try:
-        turn_off(out_port,keys.index(key.char)+1,64)
+        turn_off(out_port,keys.index(key.char)+1,20)
     except AttributeError:
         pass
 
-# Collect events until released
-with keyboard.Listener(
-        suppress=True,
-        on_press=on_press,
-        on_release=on_release) as listener:
-    listener.join()
+
+turn_off(out_port)
+mid = mido.MidiFile("test.MID")
+# for i, track in enumerate(mid.tracks):
+#     print('Track {}: {}'.format(i, track.name))
+#     for msg in track:
+#         print(msg)
+
+
+for msg in mid.tracks[0]:
+    print(msg)
+    time.sleep(msg.time)
+    out_port.send(msg)
+    
+turn_on(out_port,1,vel = 30)
+for msg in mido.MidiFile('test.mid').play():
+    print(msg)
+    out_port.send(msg)
+with mido.open_input("PD 12 1") as inport:
+    for msg in inport:
+        turn_on(out_port,msg.note,msg.velocity)
+        print(msg)
+
+# asdCollect events until released
+# with keyboard.Listener(
+#         suppress=True,
+#         on_press=on_press,
+#         on_release=on_release) as listener:
+#     listener.join()
 
